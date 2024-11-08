d8edfd75fc3697186f157240ab68ec78cfc2c78d
amoghrathod <amoghrrathod@gmail.com>
2024-11-08 11:43:53 +0530

    added chessgame as a route

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,22 +1,24 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Home from './components/Home';
-import Game1 from './components/Game1';
-import Game2 from './components/Game2';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Chess from "./components/ChessGame";
+import Game1 from "./components/Game1";
+import Game2 from "./components/Game2";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/game1" element={<Game1 />} />
-                <Route path="/game2" element={<Game2 />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/chess" element={<Chess />} />
+        <Route path="/game1" element={<Game1 />} />
+        <Route path="/game2" element={<Game2 />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
